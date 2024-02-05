b8928c9985c229b8f11c58ac8e11654e4aaf4667
Fiza Gulzar <64185194+FizaGulzarHussain@users.noreply.github.com>
2024-02-05 12:45:40 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import CTA from "./components/cta/CTA";
+import Footer from "./components/footer/Footer";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navigation-bar/navbar";
+import Testimonial from "./components/testimonial/Testimonial";
+
+function App() {
+  useEffect(() => {
+    function disableRightClick(event) {
+      event.preventDefault();
+    }
+
+    document.addEventListener("contextmenu", disableRightClick);
+
+    return () => {
+      document.removeEventListener("contextmenu", disableRightClick);
+    };
+  }, []); 
+  return (
+    <>
+      <div className="bg_color fixed inset-0 w-screen h-screen opacity-40 z-[-5]"></div>
+      <Navbar />
+      <Hero />
+      <CTA />
+      <Testimonial/>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
